4152b7558ed77679e63bea9e6e0aa14b3785fbec
TRASH-PROGRAMMER <e1316318565@live.uleam.edu.ec>
2026-05-30 19:31:08 -0500

    ui de pantalla home

--- app/src/main/java/com/example/quickorderapp/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/quickorderapp/ui/screens/home/HomeScreen.kt
@@ -1,13 +1,33 @@
 package com.example.quickorderapp.ui.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.quickorderapp.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Text
+
 
 /**
 
@@ -22,10 +42,43 @@ import androidx.navigation.NavController
 @SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun HomeScreen(navController: NavController) {
-    BoxWithConstraints(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Home Screen")
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),// Agrega padding
+        verticalArrangement = Arrangement.Top
+    ){
+        Text("MENU",
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign=TextAlign.Left ,
+            modifier = Modifier.fillMaxWidth().padding(top = 24.dp) ,
+            fontWeight = FontWeight.Bold, fontFamily=FontFamily.SansSerif,
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(top = 8.dp),
+            thickness = 2.dp,
+            color = colorResource(id = R.color.gray)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Row {
+            FilterChip(
+                selected = true,
+                onClick = { },
+                label = { Text("Entradas") }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            FilterChip(
+                selected = false,
+                onClick = { },
+                label = { Text("Platos Fuertes") }
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            FilterChip(
+                selected = false,
+                onClick = { },
+                label = { Text("Promociones") }
+            )
+        }
+        }
     }
-}
